630a2640b06a223e22f48a466a43b2121f8cead0
Maarcoos0 <marcos.pauloferreirade.moraes@escola.pr.gov.br>
2024-06-28 11:51:23 -0300

    Removendo
    Removendo documentos

---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+//Area do quarado
+
+var lado = 5;
+
+var multiplicação = lado * lado;
+console.log(multiplicação)
+
+//Perimetro de quadrado
+
+var lado = 5;
+var multiplicação = 4 * lado;
+console.log(multiplicação)
