6be8b1a21275f8fda7aef16b29bccb49f13a6fc2
Maarcoos0 <marcos.pauloferreirade.moraes@escola.pr.gov.br>
2024-06-28 11:05:59 -0300

    Arrumando
    Arrumando a pasta

---
+++ Lista de revisão/Exercício1.js
@@ -0,0 +1,15 @@
+//Area do retangulo
+
+var base = 5;
+var altura = 10;
+
+var multiplicação = base * altura;
+console.log(multiplicação)
+
+//Perimetro do retangulo
+
+var h = 5;
+var b = 8;
+
+var soma = 2 * (b + h);
+console.log(soma)
